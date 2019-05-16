e3837ef6ca186ac4bd183b349d7dd57bcd8514be
fatemeh <f.abedini1369@gmail.com>
2019-05-16 22:00:14 +0430

    talk to user and ask to play or not

--- random_number.py
+++ random_number.py
@@ -1,9 +1,20 @@
-#Random number
+#Random game:can you gues the right number
+print('hello, my name is Fatemeh.\nDo you Want to play with me?')
+user_answer=input()
+answer='yes'
+
+if user_answer== answer:
+  user_name=input('What is your name:\n')
+  print('Okay.',user_name,'lets do it.')
+else:
+  print('Whatever you want.Bye!')
+  exit()
+
 import random
 random_number=random.randint(0,101)
-Game_state=True
+Game_state=True # how many times user can guess the number
 
-while Game_state:
+while Game_state: #do the statement until Game_state is True
   guess_number=int(input('Please enter your guess:'))
   
   if guess_number < random_number:
@@ -16,7 +27,7 @@ while Game_state:
   
   if guess_number==random_number:
     print('your guess is unbelivabel')
-    break
+    break # or:Game_state=False
 
 
 
